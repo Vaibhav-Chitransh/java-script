342ed102f616593ebdddd2403414ea36c08d24ad
Vaibhav Chitransh <119576891+Vaibhav-Chitransh@users.noreply.github.com>
2024-02-05 13:28:22 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(3+2)
+console.log(3-2)
+console.log(3*2)
+console.log(3/2)
+console.log(3**2)
+console.log(3%2)
+
+let str1 = "Hello"
+let str2 = " Vaibhav"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(typeof("1" + 2))
+console.log(1 + "2")
+console.log(typeof(1 + "2"))
+console.log("1" + 2 + 2)
+console.log(typeof("1" + 2 + 2))
+console.log(1 + 2 + "2")
+console.log(typeof(1 + 2 + "2"))
+
+console.log(+true)
+// console.log(true+)  // gives the error
+console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter++)
+console.log(++gameCounter)
