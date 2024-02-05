877f58d7bb5d22ac9c04360031c3fba2f1df99dd
Vaibhav Chitransh <119576891+Vaibhav-Chitransh@users.noreply.github.com>
2024-02-05 13:07:55 +0000

    conversion in datatypes

---
+++ basics/conversionOperations.js
@@ -0,0 +1,75 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+score = null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+score = undefined
+
+console.log(typeof score)
+console.log(typeof(score))
+
+valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+score = "Vaibhav"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+score = true
+
+console.log(typeof score)
+console.log(typeof(score))
+
+valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+score = false
+
+console.log(typeof score)
+console.log(typeof(score))
+
+valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+isLoggedIn = ""
+
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
